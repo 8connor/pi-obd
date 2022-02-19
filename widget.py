0f56f31e40e082b71daa245803f9b0bccdd23d1e
The-Napster <connorh16@gmail.com>
2022-02-19 13:55:15 -0500

    Testing on pi.

--- widget.py
+++ widget.py
@@ -8,7 +8,7 @@ from PySide2.QtWidgets import QApplication, QWidget
 from PySide2.QtCore import QFile
 from PySide2.QtUiTools import QUiLoader
 
-connection = obd.Async('/dev/cu.usbserial-113011152750');
+# connection = obd.Async('/dev/cu.usbserial-113011152750');
 
 
 class Widget(QWidget):
@@ -17,26 +17,26 @@ class Widget(QWidget):
         self.load_ui()
         
 
-    def changeNum (self):
-        def new_psi(r):
-            new_num = [int(s) for s in str(r).split() if s.isdigit()]
-            display_num = round(new_num[0] * 0.14503774)
+    # def changeNum (self):
+        # def new_psi(r):
+        #     new_num = [int(s) for s in str(r).split() if s.isdigit()]
+        #     display_num = round(new_num[0] * 0.14503774)
 
-            self.findChild(QtWidgets.QLCDNumber, 'lcdNumber').display(display_num)
+        #     self.findChild(QtWidgets.QLCDNumber, 'lcdNumber').display(display_num)
 
-        def new_oil_temp(r):
-            print(r.value)
+        # def new_oil_temp(r):
+        #     print(r.value)
 
-            new_num = [int(s) for s in str(r.value.to('f')).split() if s.isdigit()]
-            print(round(new_num[0]));
+        #     new_num = [int(s) for s in str(r.value.to('f')).split() if s.isdigit()]
+        #     print(round(new_num[0]));
 
-            self.findChild(QtWidgets.QLCDNumber, 'lcdNumber_2').display()
+        #     self.findChild(QtWidgets.QLCDNumber, 'lcdNumber_2').display()
 
 
-        connection.watch(obd.commands.INTAKE_PRESSURE, callback=new_psi, force=True);
-        connection.watch(obd.commands.OIL_TEMP, callback=new_oil_temp, force=True);
+        # connection.watch(obd.commands.INTAKE_PRESSURE, callback=new_psi, force=True);
+        # connection.watch(obd.commands.OIL_TEMP, callback=new_oil_temp, force=True);
 
-        connection.start()
+        # connection.start()
 
     def load_ui(self):
         loader = QUiLoader()
@@ -50,6 +50,6 @@ class Widget(QWidget):
 if __name__ == "__main__":
     app = QApplication([])
     widget = Widget()
-    widget.changeNum()
+    # widget.changeNum()
     widget.show()
     sys.exit(app.exec_())
