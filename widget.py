2048a0d52a485906e7782bc0a8bd0a2e3b7bd514
The-Napster <connorh16@gmail.com>
2022-02-19 01:07:48 -0500

    Style changes.

--- widget.py
+++ widget.py
@@ -1,21 +1,14 @@
 # This Python file uses the following encoding: utf-8
 import os
 from pathlib import Path
-import sys
+import sys;
+import obd;
 from PySide6 import QtWidgets
 from PySide6.QtWidgets import QApplication, QWidget
 from PySide6.QtCore import QFile
 from PySide6.QtUiTools import QUiLoader
-import obd;
-import time
 
-
-# a callback that prints every new value to the console
-
-
-# print(supports)
-
-# # the callback will now be fired upon receipt of new values
+connection = obd.Async('/dev/cu.usbserial-113011152750');
 
 
 class Widget(QWidget):
@@ -25,20 +18,24 @@ class Widget(QWidget):
         
 
     def changeNum (self):
-         
-        print('HELLO WORLD!!!')
-
-        def new_rpm(r):
-            print(r.value)
-
+        def new_psi(r):
             new_num = [int(s) for s in str(r).split() if s.isdigit()]
-            print(round(new_num[0] * 0.14503774));
             display_num = round(new_num[0] * 0.14503774)
 
             self.findChild(QtWidgets.QLCDNumber, 'lcdNumber').display(display_num)
 
-        connection = obd.Async('/dev/cu.usbserial-113011152750');
-        connection.watch(obd.commands.INTAKE_PRESSURE, callback=new_rpm, force=True)
+        def new_oil_temp(r):
+            print(r.value)
+
+            new_num = [int(s) for s in str(r.value.to('f')).split() if s.isdigit()]
+            print(round(new_num[0]));
+
+            self.findChild(QtWidgets.QLCDNumber, 'lcdNumber_2').display()
+
+
+        connection.watch(obd.commands.INTAKE_PRESSURE, callback=new_psi, force=True);
+        connection.watch(obd.commands.OIL_TEMP, callback=new_oil_temp, force=True);
+
         connection.start()
 
     def load_ui(self):
