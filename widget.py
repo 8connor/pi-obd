99ef99a05aaeba31aee4959c4ed4ddd8ea14a5f7
The-Napster <connorh16@gmail.com>
2022-02-18 20:13:55 -0500

    Moving.

---
+++ widget.py
@@ -0,0 +1,51 @@
+# This Python file uses the following encoding: utf-8
+import os
+from pathlib import Path
+import sys
+from PySide6 import QtWidgets
+from PySide6.QtWidgets import QApplication, QWidget
+from PySide6.QtCore import QFile
+from PySide6.QtUiTools import QUiLoader
+import obd;
+import time
+
+
+# a callback that prints every new value to the console
+
+
+# print(supports)
+
+# # the callback will now be fired upon receipt of new values
+
+
+class Widget(QWidget):
+    def __init__(self):
+        super(Widget, self).__init__()
+        self.load_ui()
+
+    def new_rpm(r, self):
+        self.findChild(QtWidgets.QLCDNumber, 'lcdNumber').display(r.value.to('psi'))
+
+   
+
+    def load_ui(self):
+        loader = QUiLoader()
+        path = os.fspath(Path(__file__).resolve().parent / "form.ui")
+        ui_file = QFile(path)
+        ui_file.open(QFile.ReadOnly)
+        loader.load(ui_file, self)
+        ui_file.close()
+    
+    connection = obd.Async('/dev/cu.usbserial-113011152750');
+    connection.watch(obd.commands.INTAKE_PRESSURE, callback=new_rpm, force=True)
+    connection.start()
+    time.sleep(60)
+    connection.stop()
+
+if __name__ == "__main__":
+    app = QApplication([])
+    widget = Widget()
+    widget.show() 
+    obd.logger.setLevel(obd.logging.DEBUG)
+   
+    sys.exit(app.exec_())
